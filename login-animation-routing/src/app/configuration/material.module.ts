d565e0556d15c7f7c94f2b7888123dd339f665c9
Jacob Robinson <Jacob.Robinson@firstservesolutions.co.uk>
2019-03-01 17:16:50 +0000

    TSK - Login page changes

--- login-animation-routing/src/app/configuration/material.module.ts
+++ login-animation-routing/src/app/configuration/material.module.ts
@@ -1,10 +1,65 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+    MatListModule,
+    MatChipsModule,
+    MatSortModule,
+    MatIconModule,
+    MatCardModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatAutocompleteModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule
+} from '@angular/material';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        FlexLayoutModule,
+        MatCardModule,
+        MatMenuModule,
+        MatListModule,
+        MatIconModule,
+        MatSortModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatInputModule,
+        MatButtonModule,
+        MatChipsModule,
+        MatCheckboxModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatProgressSpinnerModule,
+        MatTooltipModule
+    ],
+    exports: [
+        FlexLayoutModule,
+        MatCardModule,
+        MatMenuModule,
+        MatListModule,
+        MatIconModule,
+        MatSortModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatInputModule,
+        MatButtonModule,
+        MatChipsModule,
+        MatCheckboxModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatProgressSpinnerModule,
+        MatTooltipModule
+    ],
 })
+
 export class MaterialModule { }
